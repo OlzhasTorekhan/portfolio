9ccdf7a856df7b8698e6516c64dd736389b23ad2
OlzhasTorekhan <47218682+OlzhasTorekhan@users.noreply.github.com>
2021-01-07 12:50:20 +0600

    Add files via upload

---
+++ Portfolios/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){
+    $("nav a").mPageScroll2id({
+        offset:60,
+        scrollSpeed: 1200
+        
+    });
+    $("a").mPageScroll2id({
+        offset:60,
+        scrollSpeed: 1200
+    });
+
+});
